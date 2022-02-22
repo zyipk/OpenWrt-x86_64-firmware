2e830af4e8988b15278fcdc2675eb9db87ad154d
zyipk <44421580+zyipk@users.noreply.github.com>
2022-02-23 01:13:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,6 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 git clone https://github.com/zyipk/packages package/zyipk
+
+rm -rf feed/luci/themes/luci-theme-agron
+rm -rf feed/luci/applications/luci-app-qbittorrent
